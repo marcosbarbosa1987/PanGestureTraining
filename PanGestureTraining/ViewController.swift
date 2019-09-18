3aa90b0d08091f5b3e272dd017ba79412f1817ef
Marcos Barbosa <marcosbarbos1987@live.com>
2019-09-17 21:56:23 -0300

    Initial Commit

---
+++ PanGestureTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PanGestureTraining
+//
+//  Created by Marcos Barbosa on 17/09/19.
+//  Copyright © 2019 Marcos Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
